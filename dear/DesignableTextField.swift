2c1f6b5289da563de6dc86201fd786f25c87d180
soyeonKim <ese2003@naver.com>
2018-08-14 01:58:11 +0900

    로그인, 회원가입, 메인, 마이페이지 뷰 추가

---
+++ dear/DesignableTextField.swift
@@ -0,0 +1,55 @@
+//
+//  DesignableTextField.swift
+//  dear
+//
+//  Created by MinhoCheon on 2018. 8. 14..
+//  Copyright © 2018년 ksy. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableTextField: UITextField, UITextFieldDelegate {
+
+    @IBInspectable var leftImage:UIImage? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var leftpadding:CGFloat = 0 {
+        didSet {
+            updateView()
+        }
+    }
+    
+    
+    func updateView() {
+        if let image = leftImage {
+            leftViewMode = .always
+            let imageView = UIImageView(frame: CGRect(x: leftpadding, y:0, width:24, height:24))
+            imageView.image = image
+            
+            let view = UIView(frame: CGRect(x: 16, y: 14, width: 52, height: 24))
+            view.addSubview(imageView)
+            
+            
+            leftView = view
+        }
+        
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        layer.borderWidth = 0
+    }
+    
+    
+    override func becomeFirstResponder() -> Bool {
+        layer.masksToBounds = true
+        layer.borderWidth = 1
+        super.becomeFirstResponder()
+
+        return true
+    }
+    
+}
---
+++ dear/PageViewController.swift
@@ -0,0 +1,44 @@
+////
+////  PageViewController.swift
+////  dear
+////
+////  Created by MinhoCheon on 2018. 8. 9..
+////  Copyright © 2018년 ksy. All rights reserved.
+////
+//
+//import UIKit
+//
+//class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+//    
+//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+//        
+//    }
+//    
+//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+//        
+//    }
+//    
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
