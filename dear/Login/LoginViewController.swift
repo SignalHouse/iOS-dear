c4249375305478556204674e2895bbfa32c554b4
soyeonKim <ese2003@naver.com>
2018-08-14 02:27:46 +0900

    튜토리얼, 설정, 메인 뷰 수정

---
+++ dear/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  dear
+//
+//  Created by MinhoCheon on 2018. 8. 14..
+//  Copyright © 2018년 ksy. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailText: DesignableTextField!
+    @IBOutlet weak var passwordText: DesignableTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
