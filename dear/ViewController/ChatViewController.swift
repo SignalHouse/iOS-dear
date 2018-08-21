3b95016096c602a2e2c8c69ab8be50c616c20aa7
soyeonKim <ese2003@naver.com>
2018-08-21 17:45:55 +0900

    edited Chat View

--- dear/ViewController/ChatViewController.swift
+++ dear/ViewController/ChatViewController.swift
@@ -9,18 +9,48 @@
 import UIKit
 import SocketIO
 
-class ChatViewController: UIViewController{
-    var modelChat = ChatMessageModel.ChatMessageSingleTon
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sendMessageText: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
     
+    var modelChat = ChatMessageModel.ChatMessageSingleTon
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // navigationBar Setting
+        self.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "backBtnItem")
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "backBtnItem")
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width-54, height: view.frame.height))
+        titleLabel.text = title
+        titleLabel.textColor = UIColor(red: 51/255, green: 85/255, blue: 106/255, alpha: 1)
+        titleLabel.font = UIFont(name: "Prata-Regular", size: 18)
+        self.navigationItem.titleView = titleLabel
     }
     
+    
+    /* tableview setting */
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let meCell = tableView.dequeueReusableCell(withIdentifier: "MeCell") as! ChatMeTableViewCell
+        let otherCell = tableView.dequeueReusableCell(withIdentifier: "OtherCell") as! ChatOtherTableViewCell
+        
+        if(indexPath.row % 2 != 0) { return meCell}
+        else { return otherCell}
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 130
+    }
 }
