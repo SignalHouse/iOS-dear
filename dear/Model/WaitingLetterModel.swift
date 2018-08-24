f58fd4e4b8ef293b7b88b95405f2916e0407dedc
soyeonKim <ese2003@naver.com>
2018-08-24 23:48:43 +0900

    발송 대기중 편지함 레이아웃 수정 / dummy data 추가

---
+++ dear/Model/WaitingLetterModel.swift
@@ -0,0 +1,40 @@
+//
+//  WaitingLetterModel.swift
+//  dear
+//
+//  Created by MinhoCheon on 2018. 8. 24..
+//  Copyright © 2018년 ksy. All rights reserved.
+//
+
+import Foundation
+
+struct WaitingLetterInfo: Codable {
+    let title: String
+    let content: String
+    let id: Int
+    let date: String
+    
+    init(title: String, content:String, id: Int, date: String) {
+        self.title = title
+        self.content = content
+        self.id = id
+        self.date = date
+    }
+}
+
+class WaitingLetterModel {
+    // Single ton
+    static let WaitingLetterModelSingleTon = WaitingLetterModel()
+    var arrayList: Array<WaitingLetterInfo>
+    
+    private init() {
+        self.arrayList = []
+        
+        self.arrayList.append(WaitingLetterInfo(title: "지금은 할리스", content: "ㅠㅠㅠㅠㅠㅠ소켓", id: 0, date: "18.02"))
+        self.arrayList.append(WaitingLetterInfo(title: "랄랄랄랄", content: "무슨말을 하지이이이이이", id: 1, date: "18.02"))
+        self.arrayList.append(WaitingLetterInfo(title: "흐으으음", content: "할말이 없네용", id: 2, date: "18.02"))
+        self.arrayList.append(WaitingLetterInfo(title: "지금은 할리스", content: "ㅠㅠㅠㅠㅠㅠ소켓", id: 3, date: "18.02"))
+        self.arrayList.append(WaitingLetterInfo(title: "지금은 할리스", content: "ㅠㅠㅠㅠㅠㅠ소켓", id: 4, date: "18.02"))
+    }
+    
+}
--- dear/ViewController/WaitingLetterTableViewController.swift
+++ dear/ViewController/WaitingLetterTableViewController.swift
@@ -11,18 +11,34 @@ import UIKit
 class WaitingLetterTableViewController: UITableViewController {
 
     @IBOutlet weak var remainTimeLabel: UILabel!
+    var modelWaiting = WaitingLetterModel.WaitingLetterModelSingleTon
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // for empty cell no line
+        tableView.tableFooterView = UIView()
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return modelWaiting.arrayList.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WaitingCell") as! WaitingTableViewCell
+        
+        let info = modelWaiting.arrayList[indexPath.row]
+        cell.content.text = info.content
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+        return 130
+    }
 }
